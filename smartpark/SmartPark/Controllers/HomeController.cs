50bc55a3ea4d75f8d1f360cd902d3614e6a88d33
Marcus Little <marcus.little@selu.edu>
2018-03-22 21:23:24 -0500

    project update

---
+++ smartpark/SmartPark/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartPark.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Development Team";
+             
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "How It Works";
+
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            ViewBag.Message = "Students";
+
+            return View();
+        }
+
+        public ActionResult RealTimeData()
+        {
+            return PartialView("_spots");
+        }
+    }
+}
---
+++ smartpark/SmartPark/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Net.WebSockets;
+
+[assembly: OwinStartupAttribute(typeof(SmartPark.Startup))]
+namespace SmartPark
+{
+    public partial class Startup
+    {
+
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+
+    }
+}
